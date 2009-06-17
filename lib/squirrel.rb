53535fa4ed384155792481d2e97a814f304e2a3f
twilson63 <tom@jackrussellsoftware.com>
2009-06-16 21:39:12 -0400

    update lib

--- lib/squirrel.rb
+++ lib/squirrel.rb
@@ -517,3 +517,21 @@ module Squirrel
     end
   end
 end
+
+class << ActiveRecord::Base
+  include Squirrel::Hook
+end
+
+if defined?(ActiveRecord::NamedScope::Scope)
+  class ActiveRecord::NamedScope::Scope
+    include Squirrel::NamedScopeHook
+  end
+end
+
+[ ActiveRecord::Associations::HasManyAssociation,
+  ActiveRecord::Associations::HasAndBelongsToManyAssociation,
+  ActiveRecord::Associations::HasManyThroughAssociation
+  ].each do |association_class|
+    association_class.send(:include, Squirrel::Hook)
+  end
+  
